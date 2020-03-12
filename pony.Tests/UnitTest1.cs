42895c5b1df654d7758ec19e7282a7aa5625b46c
Severin Anatoliy <severin@pointcad.ru>
2020-03-13 00:48:39 +0300

    add template unit test

---
+++ pony.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace pony.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(2 + 2 == 4);
+        }
+    }
+}
